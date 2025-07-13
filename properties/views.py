c5f8bc805c7cb8005032ae880e2094c3b3273610
Pritchad25 <pricts29@gmail.com>
2025-07-13 11:43:00 +0000

    update view to match the expected format.

--- properties/views.py
+++ properties/views.py
@@ -6,5 +6,6 @@ from properties.models import Property
 @cache_page(60 * 15)  # Cache for 15 minutes
 def property_list(request):
     properties = Property.objects.all().values('id', 'title', 'description', 'price', 'location', 'created_at')
-    return JsonResponse(list(properties), safe=False)
-# Create your views here.
+    return JsonResponse({
+        "data": list(properties)
+    })
